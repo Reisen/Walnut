c718b33c46c3da380bcd2977b7db822605117470
Reisen <reisen@morphism.org>
2014-09-21 00:20:05 +0100

    Cleaner

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,14 @@ extern crate hiredis;
 
 fn main() {
     let redis  = hiredis::Redis::new("127.0.0.1", 6379);
-
-    let result = redis.command("PSUBSCRIBE IRC.*");
+    let result = redis.command("SUBSCRIBE IRC");
 
     loop {
-        println!("\nFetching");
         let result = match redis.receive() {
             Some(v) => v,
             None    => { break; }
         };
 
-        println!(
-            "Result Type: {} -> {}",
-            result.typename() as uint,
-            result.string()
-        );
+        println!("Num: {}", result.typename() as uint);
     }
 }
