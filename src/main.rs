81842fb37a1eb65588c41079db2414f9678b7c6f
Reisen <reisen@morphism.org>
2014-09-21 02:24:54 +0100

    Latest

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,10 @@ fn main() {
     let redis  = hiredis::Redis::new("127.0.0.1", 6379);
     let result = redis.command("SUBSCRIBE IRC");
 
-    loop {
-        let result = match redis.receive() {
-            Some(v) => v,
-            None    => { break; }
-        };
+    let mut reply = hiredis::Reply::empty();
 
-        println!("Num: {}", result.typename() as uint);
+    loop {
+        redis.receive(&mut reply);
+        println!("Num: {}", reply.typename() as uint);
     }
 }
