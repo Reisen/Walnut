29ce263a385d6f196e7f6d5187939c10c1468994
Reisen <reisen@morphism.org>
2015-12-18 09:14:31 +0000

    Remove Crystal Networking

--- net/src/net/connection.cr
+++
@@ -1,9 +0,0 @@
-module Net
-    abstract class Connection
-        abstract def connect
-        abstract def disconnect
-        abstract def send
-        abstract def recv
-        abstract def conn
-    end
-end
--- net/src/net/version.cr
+++
@@ -1,3 +0,0 @@
-module Net
-  VERSION = "0.1.0"
-end
